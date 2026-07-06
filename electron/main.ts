6cb149ceef52bc57952ddf34635c3e3b3bdb4755
iGaves <1060151577@qq.com>
2026-07-06 18:41:28 +0800

    fix: correct renderer path for production build

--- electron/main.ts
+++ electron/main.ts
@@ -24,7 +24,7 @@ function createWindow() {
   if (process.env.ELECTRON_RENDERER_URL) {
     mainWindow.loadURL(process.env.ELECTRON_RENDERER_URL)
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/renderer/index.html'))
+    mainWindow.loadFile(path.join(__dirname, '../../dist/renderer/index.html'))
   }
 }
 
